dca8edc3c9435274ea07aa3056fbddb201f94715
Joren Rothman <rothmanjoren@gmail.com>
2021-11-26 22:44:17 +0100

    Basic animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import headerImage from "../public/header-image.png";
+import { motion } from "framer-motion";
 import styles from "../styles/Header.module.css";
 
 function Header() {
   return (
     <section className={styles.border}>
-      <div className={styles.backgroundDark}>
+      <motion.div
+        animate={{ y: "0%", opacity: 1 }}
+        initial={{ y: "-10%", opacity: 0 }}
+        transition={{ duration: 0.65 }}
+        className={styles.backgroundDark}
+      >
         <div className={styles.mainText}>
           <p className={styles.largeText}>Hi!</p>
           <p>
@@ -20,11 +26,22 @@ function Header() {
           <p className={styles.bingoDark}>Bingo Bango</p>
           <p className={styles.bingoLight}>Bingo Bango</p>
         </div>
-      </div>
+      </motion.div>
 
-      <div className={styles.image}>
-        <Image src={headerImage} alt="" />
-      </div>
+      <motion.div
+        className={styles.image}
+        initial={{ opacity: 0, y: 40, x: 40 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ delay: 0.85 }}
+      >
+        <motion.div
+          initial={{ x: -30, y: 30 }}
+          animate={{ x: 0, y: 0 }}
+          transition={{ delay: 1.5 }}
+        >
+          <Image src={headerImage} alt="" />
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
